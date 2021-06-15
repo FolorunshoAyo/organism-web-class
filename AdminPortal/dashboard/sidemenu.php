7caccd51804fc366ef7c79cd1b63bc10a10aecfa
FolorunshoAyo <folushoayomide11@gmail.com>
2021-06-15 15:38:01 +0100

    added all files

---
+++ AdminPortal/dashboard/sidemenu.php
@@ -0,0 +1,60 @@
+  <!-- Sidebar Menu -->
+  <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+    <!-- nav item for student -->
+              <li class="nav-item">
+                <a href="students.php" class="nav-link">
+                  <i class="far fa-user nav-icon"></i>
+                  <p>Manage Student</p>
+                </a>
+              </li>
+              <!-- nav item for students -->
+              <li class="nav-item">
+                <a href="result.php" class="nav-link">
+                  <i class="fas fa-copy"></i>
+                  <p>Students Result</p>
+                </a>
+              </li>
+              <!-- nav for student payments -->
+              <li class="nav-item">
+                <a href="payments.php" class="nav-link">
+                  <i class="fas fa-credit-card"></i>
+                  <p>Student Payments</p>
+                </a>
+              </li>
+               <!-- nav for create staff account -->
+               <li class="nav-item">
+                <a href="staffAccount.php" class="nav-link">
+                  <i class="fas fa-user-plus"></i>
+                  <p>Create Staff Account</p>
+                </a>
+              </li>
+
+               <!-- nav for manage staff account -->
+               <li class="nav-item">
+                <a href="ManageStaff.php" class="nav-link">
+                  <i class="fas fa-user"></i>
+                  <p>Manage Staff Account</p>
+                </a>
+              </li>
+
+            <!-- nav for manage staff payroll -->
+            <li class="nav-item">
+                <a href="payroll.php" class="nav-link">
+                  <i class="fas fa-user"></i>
+                  <p>Staff Payroll</p>
+                </a>
+              </li>
+              
+              
+              <!-- nav for logout -->
+                <li class="nav-item">
+                <a href="logout.php" class="nav-link">
+                  <i class="fas fa-power-off nav-icon"></i>
+                  <p>Logout </p>
+                </a>
+              </li>
+              
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
---
+++ server_scripts/login-backend.php
@@ -0,0 +1,44 @@
+<?php
+include("../connect.php");
+include('../log.php');
+session_start();
+
+$logs = new Log();
+
+if($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+  try{
+    $MatricNo = mysqli_real_escape_string($conn, $_POST['matricno']);
+    $Password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    // creating a hash (cryptography) function for password
+    // $Key = "ADULT".$Password."XTRA";
+    // $Hash = hash('sha512',$Key);
+    // $Password = md5($Hash, true);
+
+    // selecting data from database
+		$sql = "SELECT * FROM student WHERE matricno = '$MatricNo' and password = '$Password'";
+    $result = mysqli_query($conn, $sql);
+    //get records from table:
+    $record = mysqli_fetch_assoc($result);
+
+    $Row = mysqli_num_rows($result);
+		if($Row == 1)
+		{
+      //using username as my session parser
+      $_SESSION['matricno'] = $MatricNo;
+      $logs->successLog($MatricNo, $Password);
+      echo json_encode(array('success' => 1));
+		}else{
+      $logs->failedLog($MatricNo, $Password);
+           echo json_encode(array('success' => 0));
+		}
+	
+}
+catch(Exception $e )
+{
+	echo 'message'. $e->getMessage();
+}
+}
+
+?>
